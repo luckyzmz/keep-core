47170880fb7f15d26a42ab2f2575c7d4123ebf29
Tomasz Slabon <tomasz.slabon@keep.network>
2021-10-01 17:44:57 +0200

    Added enabled compiler optimiser for random beacon

--- solidity/random-beacon/hardhat.config.ts
+++ solidity/random-beacon/hardhat.config.ts
@@ -14,6 +14,11 @@ const config: HardhatUserConfig = {
     compilers: [
       {
         version: "0.8.6",
+        settings: {
+          optimizer: {
+            enabled: true,
+          },
+        },
       },
     ],
   },
